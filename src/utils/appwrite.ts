2a7a6ab5fb2a182bbf4d51f09c94087fdc69daad
Abhay Kumar <abhaykv04@gmail.com>
2024-12-07 15:40:42 +0530

    ~Appwrite

--- src/utils/appwrite.ts
+++
@@ -1,8 +0,0 @@
-import { Client, Databases } from "appwrite";
-import { projectId } from "./constants";
-
-const client = new Client();
-
-client.setEndpoint("https://cloud.appwrite.io/v1").setProject(projectId);
-
-export const db = new Databases(client);
