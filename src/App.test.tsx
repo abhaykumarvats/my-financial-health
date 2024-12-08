5b9f115a18ef71842d25dc18b578590f3e3676dc
Abhay Kumar <abhaykv04@gmail.com>
2024-12-09 02:39:18 +0530

    App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import { render } from "@testing-library/react";
-import React from "react";
 
-import App from "./app";
+import App from "./App";
 
 test("renders without crashing", () => {
   const { baseElement } = render(<App />);
